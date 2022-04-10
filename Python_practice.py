1f75c61f426bd5f56b2c556c3b244ead3b2d5031
sanilveeravu <mailsanilnow@yahoo.com>
2022-04-10 12:17:36 -0500

    updated work file reader

---
+++ Python_practice.py
@@ -0,0 +1,77 @@
+for county in counties_dict:
+     print(f"{county} county has {counties_dict[county]:,} registered voters")
+
+for county in counties_dict:
+     print(f"{county} county has {counties_dict.get(county):,} registered voters")
+
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+for county_dict in voting_data:
+     print(f'{county_dict.get("county")} county has {county_dict.get("registered_voters"):,} registered voters')
+
+for county_dict in voting_data:
+    print(county_dict)
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")   
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county, voters in counties_dict.items():
+    print(county + " county has " + str(voters) + " registered voters.")
+
+
+for num in range(5):
+    print(num)
+
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not the list of counties.")
+
+for county in counties:
+    print(county)
+
+print("Hello World")
+temperature = int(input("What is the temperature outside? "))
+
+if temperature > 80:
+    print("Turn on the AC.")
+else:
+    print("Open the windows.")
+
+
+#What is the score?
+score = int(input("What is your test score? "))
+
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+else:
+    if score >= 80:
+        print('Your grade is a B.')
+    else:
+        if score >= 70:
+            print('Your grade is a C.')
+        else:
+            if score >= 60:
+                print('Your grade is a D.')
+            else:
+                print('Your grade is an F.')    
+
+
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+elif score >= 80:
+    print('Your grade is a B.')
+elif score >= 70:
+    print('Your grade is a C.')
+elif score >= 60:
+    print('Your grade is a D.')
+else:
+    print('Your grade is an F.')
